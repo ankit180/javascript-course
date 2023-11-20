4f20f00e2a00b9e2260f18a36edfd9d6c5f07cfc
ankit180 <45695640+ankit180@users.noreply.github.com>
2023-11-20 07:51:20 +0000

    conversion of data types

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,18 @@
+let score = "33";
+let convertNumber = Number(score);
+ //conversion to a Number
+ //"33" => 33
+ // "33abc" => NaN
+ //true = 1 false = 0
+
+ let isLoggedIn = "hitesh";
+ let booleadIsLoggedIn = Boolean(isLoggedIn);
+//1 => true 0 => false
+//"" => false
+//"Something" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//33 => "33"
+
+
