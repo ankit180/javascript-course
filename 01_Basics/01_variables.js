05cb2909c5f7bf77010c4bbe5bb7275728c00274
ankit180 <45695640+ankit180@users.noreply.github.com>
2023-11-20 07:33:29 +0000

    variables and datatype

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144335; //locks the value throughout
+let accountEmail = "ankit@google.com";
+var accountCity = "Jaipur";
+/*
+Prefer not to use var
+*/
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //-> for new js versions nand treat old code as a new one it's by default but good practice
+ //number 
+ //bigint
+ //string="" (use "", more readable)
+ //boolean
+ //null -> standalone value(empty value ) it's an object
+ //undefined => value not assigned
+ //symbol  => unique
+ //object
